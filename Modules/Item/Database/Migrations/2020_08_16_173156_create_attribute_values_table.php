551dea7f201ee9d8b6596b3067e7f902155fba35
MD. Maniruzzaman Akash <akash.corp@akij.net>
2020-08-16 23:38:29 +0600

    Item Attributes Added

---
+++ Modules/Item/Database/Migrations/2020_08_16_173156_create_attribute_values_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAttributeValuesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attribute_values', function (Blueprint $table) {
+            $table->id();
+            $table->string('value');
+            $table->string('code');
+            $table->unsignedBigInteger('attribute_id');
+            $table->timestamps();
+            $table->foreign('attribute_id')->references('id')->on('attributes');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attribute_values');
+    }
+}
---
+++ Modules/Item/Entities/ItemAttribute.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Modules\Item\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ItemAttribute extends Model
+{
+    protected $fillable = [];
+}

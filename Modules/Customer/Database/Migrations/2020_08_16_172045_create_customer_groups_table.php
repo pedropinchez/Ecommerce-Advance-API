6e1a199edb6e524aab96f829c0f30be7a14314fd
MD. Maniruzzaman Akash <akash.corp@akij.net>
2020-08-16 23:22:53 +0600

    Customer Group Database added

---
+++ Modules/Customer/Database/Migrations/2020_08_16_172045_create_customer_groups_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomerGroupsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customer_groups', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('business_id');
+            $table->string('name');
+            $table->float('amount')->default(0);
+            $table->timestamps();
+            $table->foreign('business_id')->references('id')->on('business');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customer_groups');
+    }
+}
---
+++ Modules/Customer/Entities/CustomerGroup.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Modules\Customer\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CustomerGroup extends Model
+{
+    protected $fillable = [];
+}

1783b002836d2c227c6122742bf003ddec722e42
MD. Maniruzzaman Akash <akash.corp@akij.net>
2020-08-16 23:28:40 +0600

    Item Tables Customized

--- Modules/Item/Database/Migrations/2020_08_15_888718_create_items_table.php
+++ Modules/Item/Database/Migrations/2020_08_15_888718_create_items_table.php
@@ -15,43 +15,29 @@ class CreateItemsTable extends Migration
     {
         Schema::create('items', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->index();
             $table->unsignedBigInteger('business_id');
-            $table->foreign('business_id')->references('id')->on('business');
-
             $table->unsignedBigInteger('unit_id');
-            $table->foreign('unit_id')->references('id')->on('units');
-
             $table->unsignedBigInteger('brand_id')->nullable();
-            $table->foreign('brand_id')->references('id')->on('brands');
-
             $table->unsignedBigInteger('category_id')->nullable();
-            $table->foreign('category_id')->references('id')->on('categories');
-
             $table->unsignedBigInteger('sub_category_id')->nullable();
-            $table->foreign('sub_category_id')->references('id')->on('categories');
-
             $table->unsignedBigInteger('tax')->nullable();
             $table->foreign('tax')->references('id')->on('tax_rates');
             $table->enum('tax_type', ['inclusive', 'exclusive']);
             $table->boolean('enable_stock')->default(0);
             $table->unsignedBigInteger('alert_quantity');
-            $table->string('sku');
+            $table->string('sku')->index();
             $table->enum('barcode_type', ['C39', 'C128', 'EAN-13', 'EAN-8', 'UPC-A', 'UPC-E', 'ITF-14']);
             $table->string('sku_manual')->nullable()->index();
-
             $table->unsignedBigInteger('created_by');
-            $table->foreign('created_by')->references('id')->on('users');
             $table->timestamps();
 
-            //Indexing
-            $table->index('name');
-            $table->index('business_id');
-            $table->index('unit_id');
-            $table->index('category_id');
-            $table->index('brand_id');
-            $table->index('created_by');
-            $table->index('sku');
+            $table->foreign('business_id')->references('id')->on('business');
+            $table->foreign('unit_id')->references('id')->on('units');
+            $table->foreign('brand_id')->references('id')->on('brands');
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->foreign('sub_category_id')->references('id')->on('categories');
+            $table->foreign('created_by')->references('id')->on('users');
         });
     }
 
---
+++ Modules/Item/Database/Migrations/2020_08_16_172516_create_attributes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAttributesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attributes', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug');
+            $table->unique(array('id', 'slug'));
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attributes');
+    }
+}

930f9d0d82a752f84b05c571d0adace0c7f5c2bc
MD. Maniruzzaman Akash <manirujjamanakash@gmail.com>
2020-12-22 22:54:10 +0600

    Updated Upload helper

--- app/Helpers/UploadHelper.php
+++ app/Helpers/UploadHelper.php
@@ -10,39 +10,41 @@ class UploadHelper
 {
 
   /**
-   * upload Upload Any Types of File. It's not checking the file type which may be checked before pass here in validation
-   * @param  [type] $f               [request for file or not]
-   * @param  [type] $file            [pdf file]
-   * @param  [type] $name            [filename]
-   * @param  [type] $target_location [location where file will store]
-   * @return [type]                  [filename]
+   * Upload Any Types of File. 
+   * 
+   * It's not checking the file type which may be checked before pass here in validation
+   * 
+   * @param  string $f               request with file
+   * @param  binary $file            file
+   * @param  string $name            filename
+   * @param  string $target_location location where file will store
+   * @return string                  filename
    */
   public static function upload($f, $file, $name, $target_location)
   {
-
     if (Request::hasFile($f)) {
       $filename = $name . '.' . $file->getClientOriginalExtension();
       $extension = $file->getClientOriginalExtension();
       $file->move($target_location, $filename);
       return $filename;
     } else {
-      return NULL;
+      return null;
     }
   }
 
 
   /**
-   * [update file]
-   * @param  [type] $f               [request for file or not]
-   * @param  [type] $file            [pdf file]
-   * @param  [type] $name            [filename]
-   * @param  [type] $target_location [location where file will store]
-   * @param  [type] $old_location    [file location which will delete]
-   * @return [type]                  [filename]
+   * Update File
+   * @param  string $f               request with file
+   * @param  binary $file            file
+   * @param string $name             filename
+   * @param  string $target_location location where file will store
+   * @param  string $old_location    file location which will delete
+   * @return string                  filename
    */
   public static function update($f, $file, $name, $target_location, $old_location)
   {
-    //delete the old notice file
+    //delete the old file
     $target_location = $target_location . '/';
     if (File::exists($target_location . $old_location)) {
       File::delete($target_location . $old_location);
@@ -55,9 +57,9 @@ class UploadHelper
 
 
   /**
-   * [delete file]
-   * @param  [type] $location [file location that will delete]
-   * @return [type]                  [null]
+   * delete file
+   * @param  type $location file location that will delete
+   * @return type                  null
    */
   public static function deleteFile($location)
   {

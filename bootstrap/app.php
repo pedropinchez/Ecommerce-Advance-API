ac8b187f7bd5b284e635f3b9611962829f0fa089
MD. Maniruzzaman Akash <akash.corp@akij.net>
2021-02-08 15:34:59 +0600

    Removed unnecessary headers

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,7 +1,7 @@
 <?php
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: *');
-header('Access-Control-Allow-Headers: Origin, X-Requested-With,Authorization,  Content-Type, Accept');
+// header('Access-Control-Allow-Origin: *');
+// header('Access-Control-Allow-Methods: *');
+// header('Access-Control-Allow-Headers: Origin, X-Requested-With,Authorization,  Content-Type, Accept');
 
 /*
 |--------------------------------------------------------------------------
